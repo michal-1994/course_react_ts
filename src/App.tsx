c5e001ae6433d5de8e96034eb93a151ccbc05ff2
Michal <michalgrzegorczyk04@gmail.com>
2024-07-14 10:10:18 +0200

    add addGoal method

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,40 @@
 /** @format */
 
 import "./App.css";
-import CourseGoal from "./components/CourseGoal";
+import CourseGoal, { type CourseGoalProps } from "./components/CourseGoal";
 import Header from "./components/Header";
 import goalsImg from "./assets/goals.jpg";
+import { useState } from "react";
 
 function App() {
+  const [goals, setGoals] = useState<CourseGoalProps[]>([]);
+
+  const handleAddGoal = () => {
+    setGoals((prevGoals) => {
+      const newGoal: CourseGoalProps = {
+        id: Math.random(),
+        title: "Learn React + TS",
+        description: "Learn React + TS description",
+      };
+      return [...prevGoals, newGoal];
+    });
+  };
+
   return (
     <main>
       <Header image={{ src: goalsImg, alt: "A list of goals" }}>
         <h1>Your Course Goals</h1>
       </Header>
-      <CourseGoal
-        title='Learn React + TS'
-        description='Learn it from the ground up'
-      />
-      <CourseGoal
-        title='Learn React + TS'
-        description='Learn it from the ground up'
-      />
+      <button onClick={handleAddGoal}>Add Goal</button>
+      {goals.map((goal) => (
+        <li key={goal.id}>
+          <CourseGoal
+            key={goal.id}
+            title={goal.title}
+            description={goal.description}
+          />
+        </li>
+      ))}
     </main>
   );
 }
--- src/components/CourseGoal.tsx
+++ src/components/CourseGoal.tsx
@@ -1,6 +1,7 @@
 /** @format */
 
 export interface CourseGoalProps {
+  id?: number;
   title: string;
   description: string;
 }
