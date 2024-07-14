2dd2812c8828e87d8b8625f983ba4c85bd025dd0
Michal <michalgrzegorczyk04@gmail.com>
2024-07-14 12:50:21 +0200

    use useRef with TS

--- src/components/NewGoal.tsx
+++ src/components/NewGoal.tsx
@@ -1,21 +1,29 @@
 /** @format */
 
-import { type FormEvent } from "react";
+import { useRef, type FormEvent } from "react";
 
 const NewGoal = () => {
+  const goal = useRef<HTMLInputElement>(null);
+  const summary = useRef<HTMLInputElement>(null);
+
   const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
     event.preventDefault();
+
+    const enteredGoal = goal.current!.value;
+    const enteredSummary = summary.current!.value;
+
+    console.log(enteredGoal, enteredSummary);
   };
 
   return (
     <form onSubmit={handleSubmit}>
       <p>
         <label htmlFor='goal'>Your goal</label>
-        <input id='goal' type='text' />
+        <input id='goal' type='text' ref={goal} />
       </p>
       <p>
         <label htmlFor='summary'>Short summary</label>
-        <input id='summary' type='text' />
+        <input id='summary' type='text' ref={summary} />
       </p>
       <p>
         <button>Add Goal</button>
