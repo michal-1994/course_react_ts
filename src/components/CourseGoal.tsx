b8b00a92dec306a2db59a05737d287d506dd6b00
Michal <michalgrzegorczyk04@gmail.com>
2024-07-14 09:38:08 +0200

    prepare file structure

---
+++ src/components/CourseGoal.tsx
@@ -0,0 +1,15 @@
+/** @format */
+
+const CourseGoal = (props: any) => {
+  return (
+    <article>
+      <div>
+        <h2>TITLE</h2>
+        <p>DESCRIPTION</p>
+      </div>
+      <button>Delete</button>
+    </article>
+  );
+};
+
+export default CourseGoal;
